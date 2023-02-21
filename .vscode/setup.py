6bcbb24497415f9c28266f172af7092f5a0c94c1
faizan9713 <mohammadfaizan7713@gmail.com>
2023-02-21 10:40:18 +0530

    this is our first version of code

---
+++ .vscode/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_namespace_packages,setup
+
+
+set(
+  name="sensor",
+  version="0.0.1",
+  author="ineuron",
+  author_email="mohammadfaizan9713",
+  packages= find_packages(),
+  install_requires=get_requirements(), 
+
+)
--- README.md
+++ README.md
@@ -12,4 +12,5 @@ pip install -r requirements.txt
 
 ```bash
 python main.py
+
 ```
